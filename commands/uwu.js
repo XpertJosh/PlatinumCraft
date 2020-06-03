c5afa5d3b3b4c0a5caf99c855ce9a03c232f97d9
Joshua Lebert <expertjosh2000@gmail.com>
2020-06-02 22:38:10 -0400

    disabled, when invoked returns a YT video

---
+++ commands/uwu.js
@@ -0,0 +1,12 @@
+// const config = require("../config.json");
+
+// module.exports = {
+//     name: "uwu",
+//     description: "uwu",
+//     aliases: ["rap", "copypasta"],
+//     public: false,
+//     execute(message, args) {
+//         if (message.author.id !== config.ownerID) {return};
+//         message.reply("https://www.youtube.com/watch?v=h6DNdop6pD8");
+//     }
+// }
