107ce0721d7f042e8941748976ea0d8e0053882a
Joshua Lebert <expertjosh2000@gmail.com>
2020-06-21 13:10:35 -0400

    added cooldown to !purge

--- commands/purge.js
+++ commands/purge.js
@@ -5,6 +5,7 @@ module.exports = {
     description: "Removes an amount of messages from a channel.",
     aliases: ["remove"],
     usage: "[command] <amount>",
+    cooldown: 10,
     permissions: "srstaff",
     execute(message, args) {
         message.channel.bulkDelete(args[0]);
