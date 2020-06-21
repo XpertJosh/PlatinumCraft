0822ec8d4b967b3f402361ed0f0e383a15d92f72
Joshua Lebert <expertjosh2000@gmail.com>
2020-06-21 13:10:47 -0400

    completed the roll command

---
+++ commands/roll.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: "roll",
+    description: "Returns a random number between the minimum and maximum value. Default value is 1-6.",
+    aliases: ["rng", "dice"],
+    usage: "[command] <min> <max>",
+    cooldown: 1,
+    execute(message, args) {
+        function generateRandomInteger(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+        
+        var min = 1;
+        var max = 6;
+        console.log(`${min}, ${max}`);
+        // sets default values
+        if (args[0]) {min = args[0]};
+        if (args[1]) {max = args[1]};
+        console.log(`${min}, ${max}`);
+        // overrides them if values are specified.
+        
+        message.channel.send(`The result is ${generateRandomInteger(min, max)}.`);
+        // generates and sends a random integer, using the generateRandomInteger function
+    }
+}
